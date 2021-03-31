38511b8a69352526e910706bf96bbf3d57a7c019
Harshit Dixit <harshitdxt3004@gmail.com>
2021-03-31 12:41:55 +0530

    Tests for auth token updated

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,11 +1,11 @@
 const request = require('supertest');
 const app = require('../app');
-describe('Login check', () => {
+describe('Valid Login check', () => {
     it('should test for valid login id and password', async () => {
         const res = await request(app)
         .post('/api/login')
         .send({
-            email:'harshitdxt3004@gmail.com',
+            email:'dkk@gmail.com',
             password:'abcdefgh'
         })
         expect(res.statusCode).toEqual(200)
@@ -17,7 +17,7 @@ describe('Invalid Login check', () => {
         const res = await request(app)
         .post('/api/login')
         .send({
-            email:'harshitdxt3004@gmail.com',
+            email:'dkk@gmail.com',
             password:'abcdef'
         })
         expect(res.statusCode).toEqual(401);
@@ -29,6 +29,44 @@ describe('Invalid auth token check', () => {
     it('should test for access without valid auth token', async () =>{
         const res = await request(app)
         .get('/api/users')
-        .send()
+        .set({authorization:`Bearer dsnanfnan`})
+        expect(res.statusCode).toEqual(401);
+        expect(res.body).toHaveProperty('error');
+    })
+})
+
+describe('Invalid auth token check', () => {
+    it('should test for access without valid auth token', async () =>{
+        const res = await request(app)
+        .get('/api/orders')
+        .set({authorization:`Bearer dsnanfnan`})
+        expect(res.statusCode).toEqual(401);
+        expect(res.body).toHaveProperty('error');
+    })
+})
+
+describe('Invalid auth token check', () => {
+    it('should test for access without valid auth token', async () =>{
+        const res = await request(app)
+        .get('/api/expense_report')
+        .set({authorization:`Bearer dsnanfnan`})
+        expect(res.statusCode).toEqual(401);
+        expect(res.body).toHaveProperty('error');
+    })
+})
+
+describe('Valid auth token check', ()=>{
+    it('should test for access with valid auth token', async()=>{
+        const res = await request(app)
+        .post('/api/login')
+        .send({
+            email:'dkk@gmail.com',
+            password:'abcdefgh'
+        })
+        const authToken = res.body.token;
+        const getUsersRes = await request(app)
+        .get('/api/users')
+        .set({authorization:`Bearer ${authToken}`})
+        expect(getUsersRes.statusCode).toEqual(200);
     })
 })
--- tests/sample.test.js
+++ tests/sample.test.js
@@ -1,5 +1,3 @@
-
-
 describe('Sample Test', ()=>{
     it('should test that true == true', ()=>{
         expect(true).toBe(true);
